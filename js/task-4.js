ba81541de9d345685acca75888e0b63a094ffca5
Leonid Andriienko <andrienkoleonid3@gmail.com>
2026-08-08 15:45:32 +0300

    Not hard but styels im too lazy to do

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const emailValue = event.target.elements.email.value.trim();
+    const passwordValue = event.target.elements.password.value.trim();
+    if (emailValue === "" || passwordValue === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+    console.log(formData);
+    event.target.reset();
+}
